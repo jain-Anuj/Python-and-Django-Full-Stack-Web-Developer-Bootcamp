870f5b980dbbeaa74e545cedef759fb51459b179
jain-Anuj <jainanuj9415@gmail.com>
2019-08-17 23:17:02 +0530

    done django lvl five!!

---
+++ Django_Lvl_five/learning_user/basicApp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from basicApp import views
+
+
+app_name = 'basicApp'
+
+urlpatterns = [
+    path('register/',views.register, name = 'register'),
+    path('user_login/',views.user_login,name = 'user_login')
+]
